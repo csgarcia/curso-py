a8fb4d96cd4b91ab939ba752792becea6d4791d6
Carlos García <carlos@propilatam.com>
2025-10-28 00:40:57 -0600

    Refactor Selenium script to load environment variables for URL and credentials, enhancing security and flexibility

--- popular-packages/06-selenium.py
+++ popular-packages/06-selenium.py
@@ -1,25 +1,29 @@
 # dont name this file selenium.py
 # otherwise it will conflict with the actual selenium package
-
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-browser = webdriver.Chrome(options=options)
+load_dotenv()  # Loads variables from .env into environment
+
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True)
+# browser = webdriver.Chrome(options=options)
+browser = webdriver.Chrome()
 browser.implicitly_wait(10)
-browser.get('somewebsite')
+browser.get(os.environ.get("SELENIUM_TEST_URL"))
 btn = browser.find_element(By.CSS_SELECTOR, "button.button-auth")
 btn.click()
 
 user_input = browser.find_element(By.NAME, "username")
 password_input = browser.find_element(By.NAME, "password")
 
-user_input.send_keys("user")
-password_input.send_keys("password")
+user_input.send_keys(os.environ.get("SELENIUM_TEST_USERNAME"))
+password_input.send_keys(os.environ.get("SELENIUM_TEST_PASSWORD"))
 password_input.send_keys(Keys.ENTER)
 
 # if you want the full class chain include the escaped colon:
@@ -31,3 +35,5 @@ name = text.split(",")[-1].strip()
 
 assert name == "Carlos"
 print("Name extracted:", name)
+
+browser.quit()
